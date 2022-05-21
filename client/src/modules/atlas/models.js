3a1b2ab72b5296f68f189bf80ee1a8be0c507c01
Wsko-12 <vaskovladislav@gmail.com>
2022-05-21 12:40:54 +0300

    feat: add test models

--- client/src/modules/atlas/models.js
+++ client/src/modules/atlas/models.js
@@ -2,6 +2,31 @@ export default [
     {
         name:'test_model',
         folder:'',
-        file:'test.glb',
+        file:'character.glb',
+    },
+    {
+        name:'sphere',
+        folder:'',
+        file:'sphere.glb',
+    },
+    {
+        name:'polygon',
+        folder:'',
+        file:'polygon.glb',
+    },
+    {
+        name:'polygon_size',
+        folder:'',
+        file:'polygon_size.glb',
+    },
+    {
+        name:'character',
+        folder:'',
+        file:'character.glb',
+    },
+    {
+        name:'pyramid',
+        folder:'',
+        file:'pyramid.glb',
     }
 ]
